213e50136e58c39ee306f54c9cf7df1ed876288d
Talmage <Tsporter20@byui.edu>
2023-09-21 16:24:20 -0600

    Push 3
    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int magic = -1;
+
+        while (magic != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            magic = int.Parse(Console.ReadLine());
+
+            if (magicNumber > magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("That's Correct! You guessed it!");
+            }
+
+        }                    
     }
 }
